f7fc4734da3e3bb206397459c9ffcaef786bcbae
momchilmitev <momchilmitev87@gmail.com>
2021-01-23 17:20:03 +0100

    added styles for desktop version

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,7 +3,11 @@ import "./Form.scss";
 const Form = () => {
   return (
     <form className="search__form">
-      <input className="search__input" type="text" />
+      <input
+        className="search__input"
+        type="text"
+        placeholder="Search for IP or domain"
+      />
       <button className="search__btn"></button>
     </form>
   );
