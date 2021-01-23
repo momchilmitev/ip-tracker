8bfeee75eba8d395160336c0c156d0a60a513137
momchilmitev <momchilmitev87@gmail.com>
2021-01-23 16:27:12 +0100

    implemented the logic for displaying the map in map component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,7 @@ function App() {
       <h1 className="app__title">IP Address Tracker</h1>
       <Form />
       <Details />
-      <Map />
+      <Map lat={51.505} lng={-0.09} />
     </div>
   );
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,35 @@
 import "./Map.scss";
+import { useEffect } from "react";
 
-const Map = () => {
-  return <section className="map">Map</section>;
+import markerIcon from "../../assets/icons/icon-location.svg";
+
+const Map = ({ lat, lng }) => {
+  useEffect(() => {
+    const myMap = window.L.map("map").setView([lat, lng], 13);
+
+    window.L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1IjoibW9tY2hpbG1pdGV2IiwiYSI6ImNqdGQzaGlscDEzNHY0M3A0N3Q2NTZ1b3MifQ.H3ataxUnf9yXk6BH4TWEPA",
+      }
+    ).addTo(myMap);
+
+    const myIcon = window.L.icon({
+      iconUrl: markerIcon,
+      iconSize: [46, 56],
+    });
+
+    window.L.marker([lat, lng], { icon: myIcon }).addTo(myMap);
+  }, [lat, lng]);
+
+  return <section id="map" className="map"></section>;
 };
 
 export default Map;
