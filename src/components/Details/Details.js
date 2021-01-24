b8c10ccc26252ef77c55c07e254727949e2b024a
momchilmitev <momchilmitev87@gmail.com>
2021-01-24 15:40:17 +0100

    added spinner for data loading

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,23 +1,28 @@
 import "./Details.scss";
+import Spinner from "../Spinner/Spinner";
 
 const Details = ({ ipInfo }) => {
   return (
     <section className="details">
       <div className="detail">
         <p className="detail__name">IP ADDRESS</p>
-        <p className="detail__data">{ipInfo.ip}</p>
+        <p className="detail__data">{ipInfo.ip ? ipInfo.ip : <Spinner />}</p>
       </div>
       <div className="detail">
         <p className="detail__name">LOCATION</p>
-        <p className="detail__data">{ipInfo.address}</p>
+        <p className="detail__data">
+          {ipInfo.address ? ipInfo.address : <Spinner />}
+        </p>
       </div>
       <div className="detail">
         <p className="detail__name">TIMEZONE</p>
-        <p className="detail__data">UTC {ipInfo.timeZone}</p>
+        <p className="detail__data">
+          UTC {ipInfo.timeZone ? ipInfo.timeZone : <Spinner />}
+        </p>
       </div>
       <div className="detail">
         <p className="detail__name">ISP</p>
-        <p className="detail__data">{ipInfo.isp}</p>
+        <p className="detail__data">{ipInfo.isp ? ipInfo.isp : <Spinner />}</p>
       </div>
     </section>
   );
---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import "./Spinner.scss";
+
+const Spinner = () => {
+  return (
+    <span className="spinner">
+      <span></span>
+      <span></span>
+    </span>
+  );
+};
+
+export default Spinner;
